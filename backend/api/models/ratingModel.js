70ab28c7774ef3bde47d412bcc52963af7c167e4
Haoran Xu <sxu2469@gmail.com>
2024-03-13 23:46:05 -0400

    updated model for correct association

--- backend/api/models/ratingModel.js
+++ backend/api/models/ratingModel.js
@@ -1,8 +1,10 @@
 /* Rating model, representing the ratings table in database
    Access model with sequelize.models.User
 */
-const { DataTypes, Model } = require("sequelize");
+const { DataTypes, Model, Sequelize } = require("sequelize");
 const sequelize = require("../../database/db.js").sequelize;
+const User = require("./userModel.js");
+// const Dispenser = require("./dispenserModel.js");
 
 class Rating extends Model {}
 const attributes = {
@@ -12,20 +14,6 @@ const attributes = {
     primaryKey: true,
     allowNull: false,
     field: "rating_id",
-  },
-  userId: {
-    type: DataTypes.INTEGER,
-    autoIncrement: true,
-    primaryKey: true,
-    allowNull: false,
-    field: "user_id",
-  },
-  dispenserId: {
-    type: DataTypes.INTEGER,
-    autoIncrement: true,
-    primaryKey: true,
-    allowNull: false,
-    field: "dispenser_id",
   },
   ratingNum: {
     type: DataTypes.INTEGER,
@@ -38,10 +26,32 @@ const attributes = {
   },
 };
 
-module.exports = Rating.init(attributes, {
+Rating.init(attributes, {
   sequelize,
   modelName: "Rating",
   timestamps: true,
   createdAt: "timestamp",
   updatedAt: false,
 });
+/* According to documentation, defaults for One-To-One associations is SET NULL
+   for ON DELETE and CASCADE for ON UPDATE. Do not configure any specific
+   behaviors for now.
+
+   A.belongsTo(B), with the foreign key being defined in source model A
+*/
+Rating.belongsTo(User, {
+  foreignKey: {
+    type: DataTypes.INTEGER,
+    field: "user_id",
+    name: "userId",
+  },
+});
+// Rating.belongsTo(Dispenser, {
+//   foreignKey: {
+//     type: DataTypes.INTEGER,
+//     field: "dispenser_id",
+//     name: "dispenserId",
+//   },
+// });
+
+module.exports = Rating
